498b32412d0ea83dda98bc3135b17cd7987197b0
Matinmmp1381 <matinmmp1381@gmail.com>
2023-05-17 02:15:07 -0700

    add structur

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./assets/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: ["cupcake", "night", "cmyk"],
+        styled: true,
+        themes: true,
+        base: true,
+        utils: true,
+        logs: true,
+        rtl: false,
+        prefix: "",
+        darkTheme: "night",
+    },
+}
+
